7ad5622da3c1e2504904f248cf7e03af59a8f57f
Alex Efros <powerman@powerman.name>
2012-12-05 10:19:03 +0200

    simplify mkfiles

--- mkfiles/mk-idx-Inferno.sh
+++
@@ -1,6 +0,0 @@
-#!/dis/sh
-load std
-
-for (d in $MANDIRS) {
-	echo $d.idx-Inferno:V: `{ls $d | grep -v '/INDEX$'}
-}
--- mkfiles/mk-idx-os.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-for d in $MANDIRS; do
-	echo $d.idx-os:V: $(ls -1 $d/* 2>/dev/null | grep -v '/INDEX$')
-done
